8d22282eaf3e136c548fd73209e6d455ccd43cb9
elzubair <ahmed.aau3@gmail.com>
2023-09-16 11:59:43 +0300

    adding search criteria

--- src/main/java/sa/com/tree/account/statment/treecodingchallenge/service/StatementService.java
+++ src/main/java/sa/com/tree/account/statment/treecodingchallenge/service/StatementService.java
@@ -48,16 +48,12 @@ public class StatementService {
 
     private boolean isStatementInDateRange(Statement statement, LocalDate fromDate, LocalDate toDate) {
         LocalDate statementDate = LocalDate.parse(statement.getDateField(), DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        boolean isStatementInDateRange = statementDate.isAfter(fromDate.minusDays(1)) && statementDate.isBefore(toDate.plusDays(1));
-        log.info("Statement date: {}, from date: {}, to date: {}, isStatementInDateRange: {}", statementDate, fromDate, toDate, isStatementInDateRange);
-        return isStatementInDateRange;
+        return statementDate.isAfter(fromDate.minusDays(1)) && statementDate.isBefore(toDate.plusDays(1));
     }
 
     private boolean isStatementInAmountRange(Statement statement, BigDecimal fromAmount, BigDecimal toAmount) {
         BigDecimal statementAmount = SearchUtils.parseAmount(statement.getAmount());
-        boolean isStatementInAmountRange = statementAmount.compareTo(fromAmount) >= 0 && statementAmount.compareTo(toAmount) <= 0;
-        log.info("Statement amount: {}, from amount: {}, to amount: {}, isStatementInAmountRange: {}", statementAmount, fromAmount, toAmount, isStatementInAmountRange);
-        return isStatementInAmountRange;
+        return statementAmount.compareTo(fromAmount) >= 0 && statementAmount.compareTo(toAmount) <= 0;
     }
 
 }
--- src/main/java/sa/com/tree/account/statment/treecodingchallenge/validators/SearchCriteriaValidator.java
+++ src/main/java/sa/com/tree/account/statment/treecodingchallenge/validators/SearchCriteriaValidator.java
@@ -2,17 +2,37 @@ package sa.com.tree.account.statment.treecodingchallenge.validators;
 
 import org.springframework.stereotype.Component;
 import sa.com.tree.account.statment.treecodingchallenge.dto.SearchCriteriaDTO;
+import sa.com.tree.account.statment.treecodingchallenge.exception.InvalidSearchCriteriaException;
 
 @Component
 public class SearchCriteriaValidator {
 
 
     public void validate(SearchCriteriaDTO searchCriteriaDTO) {
+        // search criteria must be provided
         if (searchCriteriaDTO == null)
-            throw new RuntimeException("Invalid Search Criteria");
+            throw new InvalidSearchCriteriaException("Invalid Search Criteria");
 
+        // account id must be provided
         if (searchCriteriaDTO.getAccountId() == null || searchCriteriaDTO.getAccountId() <= 0)
-            throw new RuntimeException("Invalid Account Id");
+            throw new InvalidSearchCriteriaException("Please specify account id");
+
+        // if user provide from date then to date must be provided
+        if (searchCriteriaDTO.getFromDate() != null && searchCriteriaDTO.getToDate() == null)
+            throw new InvalidSearchCriteriaException("Please specify to date");
+
+        // if user provide to date then from date must be provided
+        if (searchCriteriaDTO.getToDate() != null && searchCriteriaDTO.getFromDate() == null)
+            throw new InvalidSearchCriteriaException("Please specify from date");
+
+        // if user provide from amount then to amount must be provided
+        if (searchCriteriaDTO.getFromAmount() != null && searchCriteriaDTO.getToAmount() == null)
+            throw new InvalidSearchCriteriaException("Please specify to amount");
+
+        // if user provide to amount then from amount must be provided
+        if (searchCriteriaDTO.getToAmount() != null && searchCriteriaDTO.getFromAmount() == null)
+            throw new InvalidSearchCriteriaException("Please specify from amount");
+
     }
 
 }
